723e20a180de522f918b2bb0b00393f8d3533480
Grisehyrdene <Dmitri.Danshin2017@gmail.com>
2021-04-15 21:23:22 +0300

    1

---
+++ resources/views/about.blade.php
@@ -0,0 +1,4 @@
+<h1>About</h1>
+<a href="javascript:history.back()">Go Back</a>
+
+<?php
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+//Route::get('/', function (){
+//   return "Hello world";
+//});
+
+Route::view('/', 'home');
+Route::view('/about', 'about');
+Route::view('/goods', 'goods');
