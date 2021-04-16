5759ebc659e9e67a84e2991e98c8d7b15d052e0d
Grisehyrdene <Dmitri.Danshin2017@gmail.com>
2021-04-16 20:00:19 +0300

    3

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Support\Facades\Date;
+
+class HomeController extends Controller
+{
+    public function index(): string
+    {
+        return view('home', ['date' =>  date('Y-m-d h:i:s')]);
+    }
+
+    public function test(): string
+    {
+        return __METHOD__;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,75 +1,12 @@
 <?php
 
+use App\Http\Controllers\{HomeController, PageController, Test\TestController};
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-
-Route::view('/', 'home')->name('home');
-Route::view('/about', 'about');
-Route::view('/goods', 'goods');
-
-
-Route::match(['get', 'post', 'put'], '/contact', function () {
-    if (!empty($_POST)) {
-        dump($_POST);
-    }
-    return view('contact');
-});
-
-/*
-Route::post('/send-email', function (){
-    if(!empty($_POST)){
-        dump($_POST);
-    }
-    return 'Send Email';
-});
-
-Route::match(['post', 'get'], '/contact', function (){
-    if(!empty($_POST)){
-        dump($_POST);
-    }
-    return view('contact');
-});
-
-Route::match(['post', 'get'], '/contact', function (){
-    if(!empty($_POST)){
-        dump($_POST);
-    }
-    return view('contact');
-})->name('contact');
-
-Route::redirect('/about','/contact');
-*/
-
-Route::get('/post/{id}', function ($id) {
-    return "Post $id";
-});
-
-Route::prefix('admin')->name('admin.')->group(function () {
-
-    Route::get('/post/create', function () {
-        return 'post create';
-    });
-
-    Route::get('/post/{id}/edit', function ($id) {
-        return "edit post $id";
-    })->name('post');
-
-    Route::get('/posts', function () {
-        return 'post list';
-    });
-
-});
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/test', [HomeController::class, 'test']);
+Route::get('/test2', [TestController::class, 'test']);
+Route::get('/{slug}', [PageController::class, 'show']);
 
 Route::fallback(function () {
     abort(404, 'Error');
