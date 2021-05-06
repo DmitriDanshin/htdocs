60c656dd519f47a5323c69645aa0c4294797d9e3
Grisehyrdene <Dmitri.Danshin2017@gmail.com>
2021-05-06 10:59:08 +0300

    6

---
+++ app/Models/Post.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+
+    // protected $table = 'my_posts';
+    // protected $primaryKey = 'post_id';
+    // public $incrementing = false;
+    // protected $keyType = 'string';
+    // public $timestamps = false;
+    protected $fillable = ['content', 'title'];
+
+
+}
